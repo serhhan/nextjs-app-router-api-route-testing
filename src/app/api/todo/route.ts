15df04ede0786bbd8e80cef651cf2b47ac639e78
Serhan Elmali <serhanelmali@gmail.com>
2023-09-18 17:34:53 +0300

    refactor todo route

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -1,5 +1,4 @@
 import { PrismaClient } from "@prisma/client";
-import type { NextApiRequest, NextApiResponse } from "next";
 import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
